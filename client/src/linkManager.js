4bb39799c9f0107ce679a79e3f3415f975329a75
Wiktor Doberschuetz <wdoberschuetz@gmail.com>
2021-06-22 14:34:01 +0200

    1701-2: Check

--- client/src/linkManager.js
+++ client/src/linkManager.js
@@ -65,7 +65,7 @@ export class linkManager extends LuigiClientBase {
     }
 
     this.options.preserveView = preserveView;
-    const relativePath = path[0] !== '/';
+    const relativePath = path[0] !== '/'; //just a comment to check out the committing
     const hasIntent = path.toLowerCase().includes('?intent=');
     const navigationOpenMsg = {
       msg: 'luigi.navigation.open',
